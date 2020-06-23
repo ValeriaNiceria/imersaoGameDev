bb752058c22f66bb1e573774f0df3ff8abc2072f
ValeriaNiceria <valerianiceria@gmail.com>
2020-06-23 09:04:56 -0300

    som do jogo

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -2,17 +2,21 @@
 let imagemCenario
 let imagemPersonagem
 let cenario
+let somDoJogo
 
 function preload() {
     // imagemCenario = loadImage('https://raw.githubusercontent.com/ValeriaNiceria/imersaoGameDev/master/assets/img/cenario/floresta.png')
     // imagemPersonagem = loadImage('https://raw.githubusercontent.com/ValeriaNiceria/imersaoGameDev/master/assets/img/personagem/correndo.png')
+    // somDoJogo = loadSound('https://raw.githubusercontent.com/ValeriaNiceria/imersaoGameDev/master/assets/music/trilha_jogo.mp3')
     imagemCenario = loadImage('assets/img/cenario/floresta.png')
     imagemPersonagem = loadImage('assets/img/personagem/correndo.png')
+    somDoJogo = loadSound('assets/music/trilha_jogo.mp3')
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
     cenario = new Cenario(imagemCenario, 3)
+    somDoJogo.loop()
 }
 
 function draw() {
