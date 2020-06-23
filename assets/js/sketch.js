783544b8339bb6c6430a163dce6023a7fe45935b
ValeriaNiceria <valerianiceria@gmail.com>
2020-06-23 09:43:44 -0300

    animação imagem

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -2,6 +2,7 @@
 let imagemCenario
 let imagemPersonagem
 let cenario
+let personagem
 let somDoJogo
 
 function preload() {
@@ -16,38 +17,13 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight)
     cenario = new Cenario(imagemCenario, 3)
+    personagem = new Personagem(imagemPersonagem)
+    frameRate(40)
     somDoJogo.loop()
 }
 
 function draw() {
     cenario.exibir()
     cenario.movimentar()
-    image(imagemPersonagem, 0, height-135, 110, 135, 0, 0, 220, 270)
-}
-
-class Cenario {
-    constructor(imagem, velocidade) {
-        this.imagem = imagem
-        this.velocidade = velocidade
-        this.x1 = 0
-        this.x2 = width
-    }
-
-    exibir() {
-        image(this.imagem, this.x1, 0, width, height)
-        image(this.imagem, this.x2, 0, width, height) 
-    }
-
-    movimentar() {
-        this.x1 = this.x1 - this.velocidade
-        this.x2 = this.x2 - this.velocidade
-
-        if (this.x1 < -width) {
-            this.x1 = width
-        }
-
-        if (this.x2 < -width) {
-            this.x2 = width
-        }
-    }
+    personagem.exibir()
 }
